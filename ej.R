5725f48a0b0b7e222a9da8d798839a5387d82a2a
juangarciaa <32419363+juangarciaa@users.noreply.github.com>
2017-10-07 13:04:00 -0500

    commit

---
+++ ej.R
@@ -0,0 +1,8 @@
+library(slidify)
+author("Juan Sebastian Garcia Arevalo")
+
+
+##Instalar, la forma clasica da un error instala desde el repositorio de github
+library(devtools)
+install_github('slidify', 'ramnathv')
+install_github("slidifyLibraries", "ramnathv")
